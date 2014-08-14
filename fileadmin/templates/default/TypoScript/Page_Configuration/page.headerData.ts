c3c199e82b47d81b7102b5bb84b1bed95a603f9b
Markus Schubath <info@mediaflair.de>
2014-08-14 09:04:13 +0200

    [FEATURE] ADD Gumby Kram

--- fileadmin/templates/default/TypoScript/Page_Configuration/page.headerData.ts
+++ fileadmin/templates/default/TypoScript/Page_Configuration/page.headerData.ts
@@ -2,16 +2,9 @@ page.headerData {
   10 = TEXT
   10 {
     field = title
-    noTrimWrap = | <title>| Firmenname</title> |
+    noTrimWrap = | <title>|</title> |
     htmlSpecialChars = 1
   }
-  
-  #20 = TEXT
-  #20.value= <!--[if IE 6]><style type="text/css">@import url(fileadmin/templates/ie6.css);</style><![endif]-->
-  
-  #30 = TEXT
-  #30.value= <!--[if lte IE 7]><style type="text/css">@import url(fileadmin/templates/ie7.css);</style><![endif]-->
-  
   #40 = TEXT
   #40.value = <link rel="apple-touch-icon" href="http://deinedomain.de/fileadmin/templates/fileadmin/templates/img/apple-touch-icon-144x144-precomposed.png" />
   45 = TEXT
@@ -24,8 +17,8 @@ page.headerData {
   50 = TEXT
   50.value = <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
-  #60 = TEXT
-  #60.value = <script gumby-touch="fileadmin/templates/js/libs" src="fileadmin/templates/js/libs/gumby.min.js"></script>
+  60 = TEXT
+  60.value = <script gumby-touch="{filepaths.scripts}libs" src="{filepaths.scripts}libs/gumby.min.js"></script>
   
   #70 = TEXT
   #70.value = <link href='http://fonts.googleapis.com/css?family=Lora:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
--- fileadmin/templates/default/TypoScript/Page_Configuration/page.includeJS.ts
+++ fileadmin/templates/default/TypoScript/Page_Configuration/page.includeJS.ts
@@ -1,11 +1,11 @@
 page {
   includeJSlibs {
-    modernizr = fileadmin/templates/js/libs/modernizr-2.6.2.min.js
-    selectivizr = fileadmin/templates/js/libs/selectivizr-min.js
-    selectivizr {
-      allWrap = <!--[if (gte IE 6)&(lte IE 8)]>|<![endif]-->
-      excludeFromConcatenation = 1
-    }
+    modernizr = {filepaths.scripts}libs/modernizr-2.6.2.min.js
+    #selectivizr = {filepaths.scripts}libs/selectivizr-min.js
+    #selectivizr {
+    #  allWrap = <!--[if (gte IE 6)&(lte IE 8)]>|<![endif]-->
+    #  excludeFromConcatenation = 1
+    #}
     jQuery = //ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js
     jQuery {
       external = 1
@@ -14,13 +14,11 @@ page {
     }
   }
   includeJSFooterlibs {
-    gumby = fileadmin/templates/js/libs/gumby.min.js
-    gumby.excludeFromConcatenation = 1
     powermailValidationEngine = EXT:powermail/Resources/Public/Js/jquery.validationEngine.js
     powermailValidationEngineEn = EXT:powermail/Resources/Public/Js/jquery.validationEngine-en.js
   }
-  includeJSFooter {
-    plugins = fileadmin/templates/js/plugins.js
-    main = {$filepaths.scripts}main.js
-  }
+  #includeJSFooter {
+  #  plugins = {filepaths.scripts}plugins.js
+  #  main = {$filepaths.scripts}main.js
+  #}
 }
