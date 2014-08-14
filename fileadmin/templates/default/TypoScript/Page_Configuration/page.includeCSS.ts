96bb55c543d29e70348b2abcd2247a7e108f42c2
Markus Schubath <info@mediaflair.de>
2014-08-14 08:33:08 +0200

    [BUGFIX] Remove old includes

--- fileadmin/templates/default/TypoScript/Page_Configuration/page.includeCSS.ts
+++ fileadmin/templates/default/TypoScript/Page_Configuration/page.includeCSS.ts
@@ -5,19 +5,3 @@ page.includeCSS {
   20 = {$filepaths.css}print.css
   20.media = print
 }
-
-# Condition to add extra CSS for Internet Explorer 6
-[browser = msie] && [version =< 7]
-page.includeCSS {
-  9999 = {$filepaths.css}msie6.css
-  9999 {
-    media = screen
-  }
-}
-[global]
-
-/*
-[globalVar = TSFE:id = 327]
-page.includeCSS.newsletterAnmeldung = {$filepaths.css}newsletter_anmeldung.css
-[global]
-*/
