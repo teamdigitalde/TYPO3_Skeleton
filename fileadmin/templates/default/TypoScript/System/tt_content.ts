846a82b24ed0c4077fe066dda65f012532e0f73e
Henrik Ziegenhain <henrik@ziegenhain.me>
2014-08-18 15:24:29 +0200

    [CLEANUP] Remove old TS-stuff

--- fileadmin/templates/default/TypoScript/System/tt_content.ts
+++ fileadmin/templates/default/TypoScript/System/tt_content.ts
@@ -1,22 +1,4 @@
 tt_content {  
-  # Make some modifications to the rendering of the standard MAIL form records
-  mailform.20 {
-    accessibility = 1
-  }
-  
-  # Remove the ancient onfocus="blurLink(this);" from sitemap links
-  # Unfortunately this hasn't been fully implemented in css_styled_content yet
-  # See also bug 11367
-  menu.20 {
-    default.1.noBlur = 1
-    1.1.noBlur = 1
-    4.1.noBlur = 1
-    5.1.noBlur = 1
-    6.1.noBlur = 1
-    7.1.noBlur = 1
-    7.2.noBlur = 1
-  }
-
   # Define different wrappers for content elements depending on the page column
   stdWrap.outerWrap.cObject = CASE
   stdWrap.outerWrap.cObject {
