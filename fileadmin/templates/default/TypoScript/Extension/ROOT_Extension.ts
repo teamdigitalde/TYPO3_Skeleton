607d8685073e0aa4a8505196b500de66fe7089e6
Henrik Ziegenhain <henrik@ziegenhain.me>
2014-09-15 08:21:14 +0200

    [FEATURE] Remove EXT:cljqueryfancybox include

--- fileadmin/templates/default/TypoScript/Extension/ROOT_Extension.ts
+++ fileadmin/templates/default/TypoScript/Extension/ROOT_Extension.ts
@@ -1,5 +1,4 @@
 ### Add TS for Extension Configuration
 <INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/Extension/plugin.gridelements.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/Extension/plugin.powermail.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/Extension/plugin.cljqueryfancybox.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/Extension/plugin.imagecycle.ts">
