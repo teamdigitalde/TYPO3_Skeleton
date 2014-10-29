1a08ae8d59823f3d1a32ffa17f09492ed37477d0
Henrik Ziegenhain <henrik@ziegenhain.me>
2014-10-29 15:28:16 +0100

    [FEATURE] Add footerData-file

--- fileadmin/templates/default/TypoScript/Page_Configuration/ROOT_Page_Configuration.ts
+++ fileadmin/templates/default/TypoScript/Page_Configuration/ROOT_Page_Configuration.ts
@@ -3,5 +3,6 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/Page_Configuration/page.config.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/Page_Configuration/page.meta.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/Page_Configuration/page.headerData.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/Page_Configuration/page.headerData.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/Page_Configuration/page.includeCSS.ts">
 <INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/Page_Configuration/page.includeJS.ts">
