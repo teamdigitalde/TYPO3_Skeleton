d36115bd13e4b95cf3630d8b6fb14cdf72d5b995
Markus Schubath <info@mediaflair.de>
2015-03-31 12:29:21 +0200

    [BUGFIX] DEL ROOT_System.ts

--- fileadmin/templates/default/TypoScript/System/ROOT_System.ts
+++
@@ -1,4 +0,0 @@
-### Add TS for System Configuration
-<INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/System/css_styled_content.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/System/lib.parseFunc_RTE.ts">
-<INCLUDE_TYPOSCRIPT: source="FILE:fileadmin/templates/default/TypoScript/System/tt_content.ts">
