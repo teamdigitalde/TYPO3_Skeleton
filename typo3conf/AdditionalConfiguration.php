b3b1be730df83ad0517985908abf9df0fa06b09c
Henrik Ziegenhain <henrik@ziegenhain.me>
2015-06-01 08:51:10 +0200

    [FEATURE] Add image button to RTE

--- typo3conf/AdditionalConfiguration.php
+++ typo3conf/AdditionalConfiguration.php
@@ -6,40 +6,43 @@ if (!defined('TYPO3_MODE')) {
 $context = \TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->__toString();
 
 $customChanges = array(
-        'FE' => array(
-                'addRootLineFields' => $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] . ',keywords,description',
-                'disableNoCacheParameter' => TRUE,
-                'hidePagesIfNotTranslatedByDefault' => TRUE
-        ),
-        'GFX' => array(
-                'png_truecolor' => 1
-        ),
-        'EXT' => array(
-                'extConf' => array(
-                        'news' => serialize(array(
-                                'removeListActionFromFlexforms' => 2,
-                                'pageModuleFieldsNews' => 'LLL:EXT:news/Resources/Private/Language /locallang_be.xml:pagemodule_simple = title,datetime;LLL:EXT:news/Resources/Private/Language/locallang_be.xml:pagemodule_advanced = title,datetime,teaser,category;LLL:EXT:news/Resources/Private/Language/locallang_be.xml:pagemodule_complex = title,datetime,teaser,category,archive;',
-                                'pageModuleFieldsCategory' => 'title,description',
-                                'tagPid' => 23,
-                                'prependAtCopy' => 0,
-                                'categoryRestriction' => 'none',
-                                'contentElementRelation' => 0,
-                                'manualSorting' => 0,
-                                'archiveDate' => 'date',
-                                'showImporter' => 0,
-                                'showAdministrationModule' => 1,
-                                'showMediaDescriptionField' => 0,
-                                'useFal' => 1,
-                        )),
-                        'realurl' => serialize(array(
-                                'configFile' => 'typo3conf/realurl_conf.php',
-                                'enableAutoConf' => 0,
-                                'autoConfFormat' => 0,
-                                'enableDevLog' => 0,
-                                'enableChashUrlDebug' => 0,
-                        )),
-                )
-        ),
+    'FE' => array(
+            'addRootLineFields' => $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] . ',keywords,description',
+            //'disableNoCacheParameter' => TRUE,
+            //'hidePagesIfNotTranslatedByDefault' => TRUE
+    ),
+    'GFX' => array(
+            'png_truecolor' => 1
+    ),
+    'EXT' => array(
+            'extConf' => array(
+                    'rtehtmlarea' => serialize(array(
+                            'enableImages' => 1,
+                    )),
+                    'news' => serialize(array(
+                            'removeListActionFromFlexforms' => 2,
+                            'pageModuleFieldsNews' => 'LLL:EXT:news/Resources/Private/Language/locallang_be.xlf:pagemodule_simple=title,datetime;LLL:EXT:news/Resources/Private/Language/locallang_be.xlf:pagemodule_advanced=title,datetime,teaser,categories;LLL:EXT:news/Resources/Private/Language/locallang_be.xlf:pagemodule_complex=title,datetime,teaser,categories,archive;',
+                            'pageModuleFieldsCategory' => 'title,description',
+                            'tagPid' => 1,
+                            'prependAtCopy' => 0,
+                            'categoryRestriction' => 'none',
+                            'contentElementRelation' => 0,
+                            'manualSorting' => 0,
+                            'archiveDate' => 'date',
+                            'showImporter' => 0,
+                            'showAdministrationModule' => 1,
+                            'showMediaDescriptionField' => 0,
+                            'useFal' => 1,
+                    )),
+                    'realurl' => serialize(array(
+                            'configFile' => 'typo3conf/realurl_conf.php',
+                            'enableAutoConf' => 0,
+                            'autoConfFormat' => 0,
+                            'enableDevLog' => 0,
+                            'enableChashUrlDebug' => 0,
+                    )),
+            )
+    ),
 );
 
 $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive($GLOBALS['TYPO3_CONF_VARS'], (array)$customChanges);
