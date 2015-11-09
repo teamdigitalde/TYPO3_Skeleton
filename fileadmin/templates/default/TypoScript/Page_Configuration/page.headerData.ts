3eba0d98a231f0a8c927d372862275299372b536
Henrik Ziegenhain <henrik@ziegenhain.me>
2015-11-09 07:57:40 +0100

    [TASK] Remove gumby.min.js reference

--- fileadmin/templates/default/TypoScript/Page_Configuration/page.headerData.ts
+++ fileadmin/templates/default/TypoScript/Page_Configuration/page.headerData.ts
@@ -130,9 +130,6 @@ page.headerData {
 		if.isTrue = {$meta.geo.active}
 	}
 	
-	60 = TEXT
-	60.value = <script gumby-touch="{$filepaths.scripts}libs" src="{$filepaths.scripts}libs/gumby.min.js"></script>
-	
 	#70 = TEXT
 	#70.value = <link href='http://fonts.googleapis.com/css?family=Lora:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
 	
