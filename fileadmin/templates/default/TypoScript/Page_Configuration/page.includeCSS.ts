c6ed9d583aa422c7617d1d04e5d263aae11abcfd
Henrik Ziegenhain <henrik@ziegenhain.me>
2015-11-10 12:21:34 +0100

    [TASK] Delete some old Gumby stuff [FEATURE] Add new bootstrap based Menu [FEATURE] Add container-class [FEATURE] Rework first level of navigation to integrate better in desktop and           mobile devices [FEATURE] Add bootstrap-resets.css [FEATURE] Add scrollToTop Icon

--- fileadmin/templates/default/TypoScript/Page_Configuration/page.includeCSS.ts
+++ fileadmin/templates/default/TypoScript/Page_Configuration/page.includeCSS.ts
@@ -1,10 +1,11 @@
 page.includeCSS {
-  #5 =  {$filepaths.css}bootstrap.min.css
-  5 =  {$filepaths.css}bootstrap.css
+  5 =  {$filepaths.css}bootstrap.min.css
   10 =  {$filepaths.css}bootstrap-theme.min.css
-  15 =  {$filepaths.css}bootstrap-submenu.css
-  20 = {$filepaths.css}styles.css
-  25 = {$filepaths.css}powermail.css
-  30 = {$filepaths.css}print.css
-  30.media = print
+  # submenu doesn open on hover, open it instead with a click
+  #20 =  {$filepaths.css}bootstrap-submenu.css
+  30 = {$filepaths.css}bootstrap-resets.css
+  50 = {$filepaths.css}styles.css
+  100 = {$filepaths.css}powermail.css
+  300 = {$filepaths.css}print.css
+  300.media = print
 }
