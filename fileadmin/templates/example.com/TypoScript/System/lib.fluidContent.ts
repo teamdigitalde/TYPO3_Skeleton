81838cb85b04f9ee7ebac91e51d194c9c09a2acb
Henrik Ziegenhain <henrik@ziegenhain.me>
2016-04-28 08:06:22 +0200

    [TASK] add title to fancyboy-Link (lib.fluidContent.ts)

---
+++ fileadmin/templates/example.com/TypoScript/System/lib.fluidContent.ts
@@ -0,0 +1,4 @@
+# Fancybox title übergeben
+lib.fluidContent.settings.media.popup {
+	linkParams.ATagParams.dataWrap = class="{$styles.content.textmedia.linkWrap.lightboxCssClass}" rel="{$styles.content.textmedia.linkWrap.lightboxRelAttribute}" title="{file:current:title}"
+}
