ef43c3c2a1ed20381cae25cce4600a67919c24ac
Henrik Ziegenhain <henrik@ziegenhain.me>
2016-05-11 07:50:08 +0200

    [BUGFIX] Remove old css & js clearing for powermail [FEATURE] Add some static data for referrer fetching in powermail

--- fileadmin/templates/example.com/TypoScript/Extension/plugin.powermail.ts
+++ fileadmin/templates/example.com/TypoScript/Extension/plugin.powermail.ts
@@ -1,14 +1,18 @@
-page.includeJSFooterlibs.powermailJQuery >
-page.includeJSFooterlibs.powermailJQueryUi >
-page.includeJSFooterlibs.powermailJQueryTabs >
-page.includeJSFooterlibs.powermailJQueryUiDatepicker >
-
-page.includeCSS.powermailJQueryUiTheme >
-page.includeCSS.powermailJQueryUiDatepicker >
-page.includeCSS.powermailCssBasic >
-
 #plugin.tx_powermail.view.templateRootPath = fileadmin/templates/default/Html/Powermail/
 #plugin.tx_powermail.view.partialRootPath = fileadmin/templates/default/Html/Powermail/
-
-page.includeJSFooterlibs.powermailJQueryFormValidation >
-page.includeJSFooterlibs.powermailJQueryFormValidationLanguage >
+/*
+plugin.tx_powermail {
+	settings.setup.prefill.referrer = TEXT
+	settings.setup.prefill.referrer {
+		typolink.parameter.data = TSFE:id
+		typolink.returnLast = URL
+	}
+}
+*/
+/*
+plugin.tx_powermail {
+  settings.setup.prefill.referrer = TEXT
+  settings.setup.prefill.referrer.data = getenv:HTTP_REFERER
+  settings.setup.optin.subject.data = LLL:fileadmin/templates/example.com/Resources/Private/Language/locallang.xlf:powermail.optin.subject
+}
+*/
