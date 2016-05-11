b9a1e67207b175ebc3000f812ecd7e7393e27d5a
Henrik Ziegenhain <henrik@ziegenhain.me>
2016-05-11 07:56:20 +0200

    [FEATURE] Remove single values from {powermail_all} marker Excluded types: hidden, captcha Excluded fields: adminonly, referrer

--- fileadmin/templates/example.com/TypoScript/Extension/plugin.powermail.ts
+++ fileadmin/templates/example.com/TypoScript/Extension/plugin.powermail.ts
@@ -16,3 +16,57 @@ plugin.tx_powermail {
   settings.setup.optin.subject.data = LLL:fileadmin/templates/example.com/Resources/Private/Language/locallang.xlf:powermail.optin.subject
 }
 */
+
+plugin.tx_powermail {
+	settings {
+		setup {
+			# Exclude values from {powermail_all} by markername or fieldtype
+			excludeFromPowermailAllMarker {
+				# On Confirmation Page (if activated)
+				confirmationPage {
+					# add some markernames (commaseparated) which should be excluded
+					excludeFromMarkerNames = adminonly, referrer
+
+					# add some fieldtypes (commaseparated) which should be excluded
+					excludeFromFieldTypes = hidden, captcha
+				}
+
+				# On Submitpage
+				submitPage {
+					# add some markernames (commaseparated) which should be excluded
+					excludeFromMarkerNames = adminonly, referrer
+
+					# add some fieldtypes (commaseparated) which should be excluded
+					excludeFromFieldTypes = hidden, captcha
+				}
+
+				# In Mail to receiver
+				receiverMail {
+					# add some markernames (commaseparated) which should be excluded
+					excludeFromMarkerNames =
+
+					# add some fieldtypes (commaseparated) which should be excluded
+					excludeFromFieldTypes =
+				}
+
+				# In Mail to sender (if activated)
+				senderMail {
+					# add some markernames (commaseparated) which should be excluded
+					excludeFromMarkerNames = adminonly, referrer
+
+					# add some fieldtypes (commaseparated) which should be excluded
+					excludeFromFieldTypes = hidden, captcha
+				}
+
+				# In double-opt-in Mail to sender (if activated)
+				optinMail {
+					# add some markernames (commaseparated) which should be excluded
+					excludeFromMarkerNames = adminonly, referrer
+
+					# add some fieldtypes (commaseparated) which should be excluded
+					excludeFromFieldTypes = hidden, captcha
+				}
+			}
+		}
+	}
+}
