cbc5876878600a7fbf9284e638a2bfe2e1ad72d1
Markus Schubath <info@mediaflair.de>
2016-07-27 11:53:47 +0200

    [BUGFIX] New Dropdown-Menu (first-four-levels.ts), overwrite Bootstrap menu-defaults (bootstrap-resets.css), change menu Slide-Out (main.js)

--- fileadmin/templates/example.com/Js/main.js
+++ fileadmin/templates/example.com/Js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 // Dropdown mit Slideout
 /*
-$('.navbar .dropdown').hover(function() {
+$('.navbar .dropdown, .dropdown-submenu ').hover(function() {
   $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
 }, function() {
   $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
