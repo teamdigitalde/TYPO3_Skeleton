664a43321a0b706c145f8ae9958db6a5b1327994
Henrik Ziegenhain <henrik@ziegenhain.me>
2016-12-07 13:13:47 +0100

    [BUGFIX] Language Menu doesn`t know anything about news-parameters etc Move lib.currentUrl to Constants.ts to allow usage all over typoscript objects

--- fileadmin/templates/example.com/TypoScript/Constants/Constants.ts
+++ fileadmin/templates/example.com/TypoScript/Constants/Constants.ts
@@ -124,3 +124,57 @@ headerData {
     tracker_url = //piwik.example.com/
   }
 }
+
+lib.currentUrl= COA
+lib.currentUrl {
+  10 = TEXT
+  10.typolink {
+    parameter.data = TSFE:id
+    returnLast = url
+    forceAbsoluteUrl = 1
+    useCacheHash = 1
+    additionalParams.cObject = COA
+    additionalParams.cObject {
+      #EXT:news
+      100 = TEXT
+      100.dataWrap = &tx_news_pi1[news]={GP:tx_news_pi1|news}
+      100.if.isTrue.data = GP:tx_news_pi1|news
+      110 = TEXT
+      110.dataWrap = &tx_news_pi1[controller]={GP:tx_news_pi1|controller}
+      110.if.isTrue.data = GP:tx_news_pi1|controller
+      120 = TEXT
+      120.dataWrap = &tx_news_pi1[action]={GP:tx_news_pi1|action}
+      120.if.isTrue.data = GP:tx_news_pi1|action
+      #EXT:cal
+      /*
+      200 = TEXT
+      200.dataWrap = &tx_cal_controller[view]={GP:tx_cal_controller|view}
+      200.if.isTrue.data = GP:tx_cal_controller|view
+      210 = TEXT
+      210.dataWrap = &tx_cal_controller[type]={GP:tx_cal_controller|type}
+      210.if.isTrue.data = GP:tx_cal_controller|type
+      220 = TEXT
+      220.dataWrap = &tx_cal_controller[uid]={GP:tx_cal_controller|uid}
+      220.if.isTrue.data = GP:tx_cal_controller|uid
+      230 = TEXT
+      230.dataWrap = &tx_cal_controller[year]={GP:tx_cal_controller|year}
+      230.if.isTrue.data = GP:tx_cal_controller|year
+      240 = TEXT
+      240.dataWrap = &tx_cal_controller[month]={GP:tx_cal_controller|month}
+      240.if.isTrue.data = GP:tx_cal_controller|month
+      250 = TEXT
+      250.dataWrap = &tx_cal_controller[day]={GP:tx_cal_controller|day}
+      250.if.isTrue.data = GP:tx_cal_controller|day
+      260 = TEXT
+      260.dataWrap = &tx_cal_controller[getdate]={GP:tx_cal_controller|getdate}
+      260.if.isTrue.data = GP:tx_cal_controller|getdate
+      270 = TEXT
+      270.dataWrap = &tx_cal_controller[categorySelection]={GP:tx_cal_controller|categorySelection}
+      270.if.isTrue.data = GP:tx_cal_controller|categorySelection
+      280 = TEXT
+      280.dataWrap = &tx_cal_controller[category][0]={GP:tx_cal_controller|category|0}
+      280.if.isTrue.data = GP:tx_cal_controller|category|9
+      */
+    }
+  }
+}
--- fileadmin/templates/example.com/TypoScript/Menu/menu.language.ts
+++ fileadmin/templates/example.com/TypoScript/Menu/menu.language.ts
@@ -5,17 +5,29 @@ menu.language {
   1 = TMENU
   1 {
     wrap = <ul>|</ul>
-    
     NO = 1
     NO {
+      doNotLinkIt = 1
       # Override the standard menu item value (which is the page title) with our own cObject
       stdWrap.cObject = TEXT
-      
-      # Use the TypoScript option split function to display different values for the first and second item
-      stdWrap.cObject.value = {$contentpage.language1} || {$contentpage.language2}
+      stdWrap.cObject {
+        # Use the TypoScript option split function to display different values for the first and second item
+        # otherwise the page title is shown
+        value = {$contentpage.language1} || {$contentpage.language2}
+        # now build the link with the defined GET-Parameters in Constants.ts
+        typolink {
+          parameter.data = TSFE:id
+          # copy authorized additionalParams
+          additionalParams < lib.currentUrl.10.typolink.additionalParams
+          additionalParams.cObject {
+            # also add L-vars (should be the same as above)
+            10 = TEXT
+            10.value = &L=0 || &L=1
+          }
+        }
+      }
       allWrap = <li>|</li>
     }
-    
     ACT < .NO
     ACT {
       ATagParams = class="active"
